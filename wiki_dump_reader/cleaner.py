dc0c179bc8f9ba010d03a647e03273f904531682
Mathieu Lecarme <mathieu@garambrogne.net>
2026-02-05 15:52:10 +0100

    Python3, typing, some linting. feat: agnostic reader, you can parse a xml.bz2 file.

--- wiki_dump_reader/cleaner.py
+++ wiki_dump_reader/cleaner.py
@@ -1,12 +1,12 @@
 import re
+from typing import Any
 
 
-class Cleaner(object):
-
+class Cleaner:
     def __init__(self):
         pass
 
-    def clean_text(self, text):
+    def clean_text(self, text: str) -> str:
         text = self._remove_file_links(text)
         text = self._remove_image_links(text)
         text = self._remove_external_links(text)
@@ -25,23 +25,23 @@ class Cleaner(object):
         text = self._remove_continuous_newlines(text)
         return text.strip()
 
-    def _remove_file_links(self, text):
+    def _remove_file_links(self, text: str) -> str:
         """Remove links like `[[File:*]]`"""
-        text = self._remove_resource_links(text, 'File')
-        text = re.sub(r'^File:.*$', '', text, flags=re.MULTILINE)
+        text = self._remove_resource_links(text, "File")
+        text = re.sub(r"^File:.*$", "", text, flags=re.MULTILINE)
         return text
 
-    def _remove_image_links(self, text):
+    def _remove_image_links(self, text: str):
         """Remove links like `[[File:*]]`"""
-        return self._remove_resource_links(text, 'Image')
+        return self._remove_resource_links(text, "Image")
 
-    def _remove_resource_links(self, text, resource):
+    def _remove_resource_links(self, text: str, resource):
         """Remove links likes `[[*:*]]`"""
-        pattern = '[[' + resource + ':'
+        pattern = "[[" + resource + ":"
         pattern_begin = text.find(pattern)
         if pattern_begin == -1:
             return text
-        begin, removed = 0, ''
+        begin, removed = 0, ""
         while begin < len(text):
             if pattern_begin > begin:
                 removed += text[begin:pattern_begin]
@@ -49,9 +49,9 @@ class Cleaner(object):
             while pattern_end < len(text):
                 ch = text[pattern_end]
                 pattern_end += 1
-                if ch == '[':
+                if ch == "[":
                     depth += 1
-                elif ch == ']':
+                elif ch == "]":
                     depth -= 1
                     if depth == 0:
                         break
@@ -67,51 +67,56 @@ class Cleaner(object):
             removed += text[begin:]
         return removed
 
-    def _remove_external_links(self, text):
+    def _remove_external_links(self, text: str) -> str:
         """Remove links like [*]"""
-        return re.sub(r'\[h[^ ]+ (.*?)\]', r'\1', text)
+        return re.sub(r"\[h[^ ]+ (.*?)\]", r"\1", text)
 
-    def _remove_refs(self, text):
+    def _remove_refs(self, text: str) -> str:
         """Remove patterns like <ref*>*</ref>"""
-        text = re.sub(r'<ref[^/]*?/>', '', text, flags=re.IGNORECASE | re.DOTALL)
-        text = re.sub(r'<ref.*?</ref>', '', text, flags=re.IGNORECASE | re.DOTALL)
+        text = re.sub(r"<ref[^/]*?/>", "", text, flags=re.IGNORECASE | re.DOTALL)
+        text = re.sub(r"<ref.*?</ref>", "", text, flags=re.IGNORECASE | re.DOTALL)
         # text = re.sub(r'{{Refbegin.*?Refend}}', '', text, flags=re.IGNORECASE | re.DOTALL)
         return text
 
-    def _remove_emphasises(self, text):
+    def _remove_emphasises(self, text: str) -> str:
         """Remove patterns like '''*'''"""
-        text = re.sub(r"'''(.*?)'''", r'\1', text, flags=re.DOTALL)
-        text = re.sub(r"''(.*?)''", r'\1', text, flags=re.DOTALL)
+        text = re.sub(r"'''(.*?)'''", r"\1", text, flags=re.DOTALL)
+        text = re.sub(r"''(.*?)''", r"\1", text, flags=re.DOTALL)
         return text
 
-    def _remove_comments(self, text):
+    def _remove_comments(self, text: str) -> str:
         """Remove patterns like <!--*-->"""
-        return re.sub(r'<!--.*?-->', '', text, flags=re.DOTALL)
+        return re.sub(r"<!--.*?-->", "", text, flags=re.DOTALL)
 
-    def _remove_langs(self, text):
+    def _remove_langs(self, text: str) -> str:
         """Remove pattenrs like {{lang-*|*}}}"""
-        return re.sub(r'{{lang(-|\|).*?\|(.*?)}}', r'\2', text, flags=re.IGNORECASE | re.DOTALL)
+        return re.sub(
+            r"{{lang(-|\|).*?\|(.*?)}}", r"\2", text, flags=re.IGNORECASE | re.DOTALL
+        )
 
-    def _remove_titles(self, text):
+    def _remove_titles(self, text: str) -> str:
         """Remove patterns like ==*=="""
-        return re.sub(r'(={2,6})\s*(.*?)\s*\1', r'\2', text)
+        return re.sub(r"(={2,6})\s*(.*?)\s*\1", r"\2", text)
 
-    def _remove_choices(self, text):
+    def _remove_choices(self, text: str) -> str:
         """Remove patterns like -{zh-hans:*; zh-hant:*}-"""
         text = re.sub(
-            r'-{.{,100}?zh(-hans|-cn|-hk|):(.{,100}?)(;.{,100}?}-|}-)',
-            r'\2', text,
-            flags=re.DOTALL
+            r"-{.{,100}?zh(-hans|-cn|-hk|):(.{,100}?)(;.{,100}?}-|}-)",
+            r"\2",
+            text,
+            flags=re.DOTALL,
         )
-        text = re.sub(r'-{.{,100}?:(.{,100}?)(;.{,100}?}-|}-)', r'\1', text, flags=re.DOTALL)
-        text = re.sub(r'-{(.{,100}?)}-', r'\1', text, flags=re.DOTALL)
+        text = re.sub(
+            r"-{.{,100}?:(.{,100}?)(;.{,100}?}-|}-)", r"\1", text, flags=re.DOTALL
+        )
+        text = re.sub(r"-{(.{,100}?)}-", r"\1", text, flags=re.DOTALL)
         return text
 
-    def _remove_templates(self, text):
+    def _remove_templates(self, text: str):
         """Remove patterns like {{*}}"""
-        begin, removed = 0, ''
+        begin, removed = 0, ""
         while begin < len(text):
-            pattern_begin = text.find('{{', begin)
+            pattern_begin = text.find("{{", begin)
             if pattern_begin == -1:
                 if begin == 0:
                     removed = text
@@ -124,46 +129,50 @@ class Cleaner(object):
             while pattern_end < len(text):
                 ch = text[pattern_end]
                 pattern_end += 1
-                if ch == '{':
+                if ch == "{":
                     depth += 1
-                elif ch == '}':
+                elif ch == "}":
                     depth -= 1
                     if depth == 0:
-                        link = text[pattern_begin + 2:pattern_end - 2]
-                        parts = link.split('|')
-                        template_type = parts[0].split(' ')[0].lower()
+                        link = text[pattern_begin + 2 : pattern_end - 2]
+                        parts = link.split("|")
+                        template_type = parts[0].split(" ")[0].lower()
                         if len(parts) == 1:
-                            if all(map(lambda x: x in {'"', "'", ' '}, parts[0][:])):
-                                removed += parts[0].replace(' ', '')
+                            if all(map(lambda x: x in {'"', "'", " "}, parts[0][:])):
+                                removed += parts[0].replace(" ", "")
                         elif len(parts) in [2, 3]:
-                            if template_type in {'le'} or template_type.startswith('link-'):
+                            if template_type in {"le"} or template_type.startswith(
+                                "link-"
+                            ):
                                 removed += parts[1]
                         break
             begin = pattern_end
         return removed
 
-    def _remove_htmls(self, text):
-        return re.sub(r'<(.*?)>', '', text, flags=re.DOTALL)
+    def _remove_htmls(self, text: str) -> str:
+        return re.sub(r"<(.*?)>", "", text, flags=re.DOTALL)
 
-    def _remove_lists(self, text):
-        return re.sub(r'^\s*[\*#]\s*', '', text, flags=re.MULTILINE)
+    def _remove_lists(self, text: str) -> str:
+        return re.sub(r"^\s*[\*#]\s*", "", text, flags=re.MULTILINE)
 
-    def _remove_indents(self, text):
-        return re.sub(r'^\s*[:;]\s*', '', text, flags=re.MULTILINE)
+    def _remove_indents(self, text: str) -> str:
+        return re.sub(r"^\s*[:;]\s*", "", text, flags=re.MULTILINE)
 
-    def _remove_styles(self, text):
-        return re.sub(r':?{\| (style|class)=.*?\|}', '', text, flags=re.IGNORECASE | re.DOTALL)
+    def _remove_styles(self, text: str) -> str:
+        return re.sub(
+            r":?{\| (style|class)=.*?\|}", "", text, flags=re.IGNORECASE | re.DOTALL
+        )
 
-    def _remove_spaces(self, text):
-        return text.replace('\u200b', '')
+    def _remove_spaces(self, text: str) -> str:
+        return text.replace("\u200b", "")
 
-    def _remove_continuous_newlines(self, text):
-        return re.sub(r'\n{2,}', '\n', text)
+    def _remove_continuous_newlines(self, text: str) -> str:
+        return re.sub(r"\n{2,}", "\n", text)
 
-    def build_links(self, text):
-        begin, removed, links = 0, '', []
+    def build_links(self, text: str) -> tuple[str, list[dict[str, Any]]]:
+        begin, removed, links = 0, "", list[dict[str, Any]]()
         while begin < len(text):
-            pattern_begin = text.find('[[', begin)
+            pattern_begin = text.find("[[", begin)
             if pattern_begin == -1:
                 if begin == 0:
                     removed = text
@@ -176,38 +185,44 @@ class Cleaner(object):
             while pattern_end < len(text):
                 ch = text[pattern_end]
                 pattern_end += 1
-                if ch == '[':
+                if ch == "[":
                     depth += 1
-                elif ch == ']':
+                elif ch == "]":
                     depth -= 1
                     if depth == 0:
-                        link = text[pattern_begin + 2:pattern_end - 2]
-                        parts = link.split('|')
+                        link = text[pattern_begin + 2 : pattern_end - 2]
+                        parts = link.split("|")
                         if len(parts) == 1:
-                            if ':' in link:
-                                pure = link.split(':')[-1]
-                                links.append({
-                                    'begin': len(removed),
-                                    'end': len(removed) + len(pure),
-                                    'link': link,
-                                    'text': pure
-                                })
+                            if ":" in link:
+                                pure = link.split(":")[-1]
+                                links.append(
+                                    {
+                                        "begin": len(removed),
+                                        "end": len(removed) + len(pure),
+                                        "link": link,
+                                        "text": pure,
+                                    }
+                                )
                                 removed += pure
                             else:
-                                links.append({
-                                    'begin': len(removed),
-                                    'end': len(removed) + len(text),
-                                    'link': link,
-                                    'text': link
-                                })
+                                links.append(
+                                    {
+                                        "begin": len(removed),
+                                        "end": len(removed) + len(text),
+                                        "link": link,
+                                        "text": link,
+                                    }
+                                )
                                 removed += link
                         elif len(parts) == 2:
-                            links.append({
-                                'begin': len(removed),
-                                'end': len(removed) + len(parts[1]),
-                                'link': parts[0],
-                                'text': parts[1]
-                            })
+                            links.append(
+                                {
+                                    "begin": len(removed),
+                                    "end": len(removed) + len(parts[1]),
+                                    "link": parts[0],
+                                    "text": parts[1],
+                                }
+                            )
                             removed += parts[1]
                         break
             begin = pattern_end
--- wiki_dump_reader/loader.py
+++ wiki_dump_reader/loader.py
@@ -1,35 +1,34 @@
-import codecs
+from typing import Generator
 from xml.etree import ElementTree
 
 
-def iterate(file_path):
-    with codecs.open(file_path, 'r', 'utf8') as reader:
-        content = None
-        for line in reader:
-            line = line.strip()
-            if line == '<page>':
-                content = [line]
-            elif line == '</page>':
+def iterate(reader) -> Generator[tuple[str | None, str], None, None]:
+    content = list[str]()
+    for line in reader:
+        line: str = line.strip()
+        if line == "<page>":
+            content = [line]
+        elif line == "</page>":
+            content.append(line)
+            content = "\n".join(content)
+            tree = ElementTree.fromstring(content)
+            content = None
+            ns_elem = tree.find("ns")
+            if ns_elem is None:
+                continue
+            if ns_elem.text.strip() != "0":
+                continue
+            title_elem = tree.find("title")
+            if title_elem is None:
+                continue
+            title = title_elem.text
+            text_elem = tree.find("revision/text")
+            if text_elem is None:
+                continue
+            text = text_elem.text
+            if text is None:
+                continue
+            yield title, text
+        else:
+            if type(content) is list:
                 content.append(line)
-                content = '\n'.join(content)
-                tree = ElementTree.fromstring(content)
-                content = None
-                ns_elem = tree.find('ns')
-                if ns_elem is None:
-                    continue
-                if ns_elem.text.strip() != '0':
-                    continue
-                title_elem = tree.find('title')
-                if title_elem is None:
-                    continue
-                title = title_elem.text
-                text_elem = tree.find('revision/text')
-                if text_elem is None:
-                    continue
-                text = text_elem.text
-                if text is None:
-                    continue
-                yield title, text
-            else:
-                if type(content) is list:
-                    content.append(line)
