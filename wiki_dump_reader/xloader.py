882f0881576fb6492ff72f208be5cdcb3242ebc9
Mathieu Lecarme <mathieu@garambrogne.net>
2026-02-12 19:39:06 +0100

    Expat all the things!

---
+++ wiki_dump_reader/xloader.py
@@ -0,0 +1,79 @@
+import io
+from typing import Generator
+from xml.parsers.expat import ParserCreate
+
+
+class Page:
+    title: str
+    text: str
+
+
+class Iterate:
+    def __init__(self, reader) -> None:
+        self.reader = reader
+        self.parser = ParserCreate()
+        self.parser.StartElementHandler = self.start_element
+        self.parser.EndElementHandler = self.end_element
+        self.parser.CharacterDataHandler = self.char_data
+        self.ready = []
+        self.slugs: list[str] = []
+        self.text_buffer = io.StringIO()
+        self.id_buffer = io.StringIO()
+        self.title_buffer = io.StringIO()
+
+    def start_element(self, name, attrs):
+        self.slugs.append(name)
+
+    def end_element(self, name):
+        # print(self.slugs)
+        if self.slugs == ["mediawiki", "page", "id"]:
+            self.id_buffer.seek(0)
+            self.id = self.id_buffer.read()
+            self.id_buffer.seek(0)
+
+        elif self.slugs == ["mediawiki", "page", "revision"]:
+            self.text_buffer.seek(0)
+            self.id_buffer.seek(0)
+            self.title_buffer.seek(0)
+            self.ready.append(
+                (
+                    self.id_buffer.read(),
+                    self.title_buffer.read(),
+                    self.text_buffer.read(),
+                )
+            )
+            self.text_buffer = io.StringIO()
+            self.id_buffer = io.StringIO()
+            self.title_buffer = io.StringIO()
+
+        self.slugs.pop()
+
+    def char_data(self, data):
+        if self.slugs == ["mediawiki", "page", "id"]:
+            self.id_buffer.write(data)
+            return
+        if self.slugs == ["mediawiki", "page", "title"]:
+            self.title_buffer.write(data)
+            return
+        if self.slugs == ["mediawiki", "page", "revision", "text"]:
+            self.text_buffer.write(data)
+
+    def loop(self):
+        while True:
+            chunk = self.reader.read(8192)
+            self.parser.Parse(chunk)
+            while len(self.ready) > 0:
+                yield self.ready.pop()
+
+
+def iterate(reader) -> Generator[str, None, None]:
+    iterator = Iterate(reader)
+    for page in iterator.loop():
+        yield page
+
+
+if __name__ == "__main__":
+    import sys
+
+    for id_, title, text in iterate(sys.stdin):
+        print(id_, title)  # , "\n\t", text[:100])
